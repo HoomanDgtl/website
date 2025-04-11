8988d9fbabf4a357ceda221631363ed82afae43a
dharamveergit <dharamveersingh.em@gmail.com>
2025-04-11 17:47:09 +0530

    refactor: update images and enhance Location component for Akash Accelerate 2025
    - Removed the old mobile image and added a new speaker image for Jake Brukhman.
    - Updated the Location component to replace static images with an embedded Google Map for improved interactivity.
    - Adjusted image paths in the speakers component to reflect the new image format.
    - Enhanced layout and styling for better responsiveness and visual consistency.

--- src/components/acc-2025/Location.tsx
+++ src/components/acc-2025/Location.tsx
@@ -12,19 +12,15 @@ const Location = () => {
       onMouseEnter={() => setIsHovering(true)}
       onMouseLeave={() => setIsHovering(false)}
     >
-      <div className="relative w-full max-w-[758px] overflow-hidden">
-        <img
-          src="/images/acc-2025/map.png"
-          alt="Akash Accelerate 2025 Location"
-          className={`relative z-[1] w-full  transform transition-all duration-500 ease-in-out ${isHovering ? " opacity-0" : " opacity-100"}`}
-        />
-        <img
-          src="/images/acc-2025/map-hover.png"
-          alt="Akash Accelerate 2025 Location Highlighted"
-          className={`absolute inset-0 z-[2] w-full  transform transition-all duration-500 ease-in-out ${isHovering ? " opacity-100" : " opacity-0"}`}
-        />
+      <div className="relative w-full flex-1 overflow-hidden">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.804551508223!2d-73.9578208!3d40.7223189!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25981a9364305%3A0x183e815910875204!2s74Wythe!5e0!3m2!1sen!2sin!4v1744373270939!5m2!1sen!2sin"
+          width="100%"
+          height="550"
+          loading="lazy"
+        ></iframe>
       </div>
-      <div className="flex w-full flex-col gap-5 bg-[#1E1E1E] px-4 py-10 md:gap-10 md:p-10">
+      <div className="flex  w-full flex-1 flex-col gap-5 bg-[#1E1E1E] px-4 py-10 md:gap-10 md:p-10">
         <div className="flex flex-col gap-3">
           <svg
             viewBox="0 0 60 61"
