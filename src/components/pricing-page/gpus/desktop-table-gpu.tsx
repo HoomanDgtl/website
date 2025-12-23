e5d5fa8d99cedb637359b72a544ed5e7c2f9a69a
dharamveergit <dharamveersingh.em@gmail.com>
2025-12-23 13:00:03 +0530

    fix: update styling for GPU table component

--- src/components/pricing-page/gpus/desktop-table-gpu.tsx
+++ src/components/pricing-page/gpus/desktop-table-gpu.tsx
@@ -256,7 +256,7 @@ const DesktopTableGpu = ({
                             variant: "secondary",
                             size: "sm",
                           }),
-                          "flex h-auto items-center gap-1.5 rounded-md border px-2 py-[5px] text-xs font-medium   md:px-2 lg:px-3",
+                          "flex h-auto items-center gap-1.5 rounded-md border px-2 py-[5px] text-xs font-medium dark:text-black   md:px-2 lg:px-3",
                         )}
                       />
                     </div>
